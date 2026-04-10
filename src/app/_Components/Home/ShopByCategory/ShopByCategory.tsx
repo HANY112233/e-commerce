0d756cff070ea387211ece09439a853d73dd357b
HANY112233 <hanyfayad.2002@gmail.com>
2026-04-10 03:28:05 +0200

    fix: handle undefined categories

--- src/app/_Components/Home/ShopByCategory/ShopByCategory.tsx
+++ src/app/_Components/Home/ShopByCategory/ShopByCategory.tsx
@@ -3,7 +3,7 @@ import { getAllCategory } from '@/api/services/routemisr.service';
 import ShopByCategoryClient from './ShopByCategoryClient';
 
 export default async function ShopByCategory() {
-  const allCategory = await getAllCategory();
+  const allCategory = (await getAllCategory()) ?? [];
 
   return <ShopByCategoryClient allCategory={allCategory} />;
 }
