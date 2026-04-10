933d71a5843eb1aa3cd09a8f49f50d639444f5d7
HANY112233 <hanyfayad.2002@gmail.com>
2026-04-10 02:40:46 +0200

    first commit

---
+++ src/app/_Components/Home/ShopByCategory/ShopByCategory.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { getAllCategory } from '@/api/services/routemisr.service';
+import ShopByCategoryClient from './ShopByCategoryClient';
+
+export default async function ShopByCategory() {
+  const allCategory = await getAllCategory();
+
+  return <ShopByCategoryClient allCategory={allCategory} />;
+}
---
+++ src/utilities.ts
@@ -0,0 +1,25 @@
+import { decode } from 'next-auth/jwt';
+import { cookies } from 'next/headers';
+
+export async function getAccessToken() {
+  const cookieStore = await cookies();
+
+  const tokenDev = cookieStore.get('next-auth.session-token')?.value;
+  const tokenProd = cookieStore.get('__Secure-next-auth.session-token')?.value;
+
+  const myToken = tokenDev || tokenProd;
+  if (!myToken) {
+    console.log('No session token found');
+    return null;
+  }
+  try {
+    const decodedToken = await decode({
+      token: myToken,
+      secret: process.env.NEXTAUTH_SECRET!,
+    });
+    return decodedToken?.routeToken || null;
+  } catch (err) {
+    console.error('Decode failed:', err);
+    return null;
+  }
+}
