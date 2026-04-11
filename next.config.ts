3ee2cae10f60603b11045eb6fdd85d84fbb6e42d
HANY112233 <hanyfayad.2002@gmail.com>
2026-04-11 13:19:29 +0200

    fix next config and categories error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   // (https://ecommerce.routemisr.com/Route-Academy-brands/1678286824747.png
-  reactCompiler: true,
+
   images: {
     remotePatterns: [
       {
